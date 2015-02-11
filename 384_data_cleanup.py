f25b5128a752eca779dacb91f393755d8fa57ff2
Richard Grant <regrant@gmail.com>
2015-02-10 21:59:33 -0500

    Now able to read CSV, remove duplicates and irrelevant lines.

--- 384_data_cleanup.py
+++ 384_data_cleanup.py
@@ -2,14 +2,33 @@ __author__ = 'regrant'
 
 import csv
 
-tab_csv = open("/Users/regrant/GDL Code/Source Files/HI DD/ALL 384 well data HHT-2994 a.csv", 'rb')
-reader = csv.reader(tab_csv, dialect='excel-tab')
 
-output_csv = open("/Users/regrant/GDL Code/Source Files/HI DD/test output.csv", 'rb')
-writer = csv.writer(output_csv, dialect='excel-tab')
+def remove_dup_and_blank(reader):
+    """
+    Cleans data given by genescan 384 well to format useful to GDL.
+    :param writer: a csv module write object
+    :return: a dictionary {patient1: {gene1: [RQ, RQ_Min, RQ_Max], gene2: [RQ, RQ_Min, RQ_Max]},
+                           patient2: {gene1: [RQ, RQ_Min, RQ_Max], gene2: [RQ, RQ_Min, RQ_Max]}}
+    """
+    data_dict = {}
+    for sample, gene, rq, rmin, rmax in reader:
+        if reader.line_num > 38:
+            # print reader.line_num
+            if sample not in data_dict and rq:
+                data_dict[sample] = {gene: [rq, rmin, rmax]}
+            elif sample in data_dict and gene not in data_dict[sample]:
+                data_dict[sample][gene] = [rq, rmin, rmax]
+    # for item, value in data_dict.items(): print item, value
+    return data_dict
 
-for row in reader
+tab_csv = open("/Users/regrant/GDL Code/Source Files/HI DD/ALL 384 well data HHT-2994 b.csv", 'rb')
+t_reader = csv.reader(tab_csv, dialect='excel-tab')
+
+t_output_csv = open("test_output.csv", 'wb')
+writer = csv.writer(t_output_csv, dialect='excel-tab')
+x = remove_dup_and_blank(t_reader)
+
 
 
 tab_csv.close()
-output_csv.close()
+t_output_csv.close()
