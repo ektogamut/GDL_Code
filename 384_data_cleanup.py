d19b6ad79c0f53c5af3fe36c5aaf5c64db5ec396
Richard Grant <regrant@gmail.com>
2015-02-11 22:20:06 -0500

    Changed from dictionary to set for duplication removal. Added flagging for outliers.

--- 384_data_cleanup.py
+++ 384_data_cleanup.py
@@ -3,6 +3,25 @@ __author__ = 'regrant'
 import csv
 
 
+# def remove_dup_and_blank(reader):
+#     """
+#     Cleans data given by genescan 384 well to format useful to GDL.
+#     :param writer: a csv module write object
+#     :return: a dictionary {patient1: {gene1: [RQ, RQ_Min, RQ_Max], gene2: [RQ, RQ_Min, RQ_Max]},
+#                            patient2: {gene1: [RQ, RQ_Min, RQ_Max], gene2: [RQ, RQ_Min, RQ_Max]}}
+#     """
+#     data_dict = {}
+#     for sample, gene, rq, rmin, rmax in reader:
+#         if reader.line_num > 38:
+#             # print reader.line_num
+#             if sample not in data_dict and rq:
+#                 data_dict[sample] = {gene: [rq, rmin, rmax]}
+#             elif sample in data_dict and gene not in data_dict[sample] and rq:
+#                 data_dict[sample][gene] = [rq, rmin, rmax]
+#     # for item, value in data_dict.items(): print item, value
+#     return data_dict
+
+
 def remove_dup_and_blank(reader):
     """
     Cleans data given by genescan 384 well to format useful to GDL.
@@ -10,16 +29,15 @@ def remove_dup_and_blank(reader):
     :return: a dictionary {patient1: {gene1: [RQ, RQ_Min, RQ_Max], gene2: [RQ, RQ_Min, RQ_Max]},
                            patient2: {gene1: [RQ, RQ_Min, RQ_Max], gene2: [RQ, RQ_Min, RQ_Max]}}
     """
-    data_dict = {}
+    data_set = set([])
     for sample, gene, rq, rmin, rmax in reader:
         if reader.line_num > 38:
-            # print reader.line_num
-            if sample not in data_dict and rq:
-                data_dict[sample] = {gene: [rq, rmin, rmax]}
-            elif sample in data_dict and gene not in data_dict[sample] and rq:
-                data_dict[sample][gene] = [rq, rmin, rmax]
+            if rq:
+                data_set.add((sample, gene, rq, rmin, rmax))
+
     # for item, value in data_dict.items(): print item, value
-    return data_dict
+    return data_set
+
 
 def write_data_dict(data_dict):
     """
@@ -36,13 +54,42 @@ def write_data_dict(data_dict):
 
     t_output_csv.close()
 
+
+def write_data(data_set):
+    """
+
+    :param data_set:
+    :return:
+    """
+    t_output_csv = open("test_output.csv", 'wb')
+    writer = csv.writer(t_output_csv, dialect='excel-tab')
+    outlier_set = set([])
+    for row in data_set:
+        writer.writerow(row)
+        if row[2] == 'RQ':
+            continue
+        rq, rmin, rmax = float(row[2]), float(row[3]), float(row[4])
+        if abs(rq - rmin) > 0.2 or abs(rq - rmax) > 0.2:
+            outlier_set.add(row)
+
+    writer.writerow([])
+    writer.writerow(['Outlier Samples: |RQ - Rmin| > 0.2 or |RQ - Rmax| > 0.2'])
+    for outlier in outlier_set:
+        writer.writerow(outlier)
+
+    t_output_csv.close()
+
+
 tab_csv = open("/Users/regrant/GDL Code/Source Files/HI DD/ALL 384 well data HHT-2994 c.csv", 'rb')
 t_reader = csv.reader(tab_csv, dialect='excel-tab')
 
 # t_output_csv = open("test_output.csv", 'wb')
 # writer = csv.writer(t_output_csv, dialect='excel-tab')
 x = remove_dup_and_blank(t_reader)
-write_data_dict(x)
+
+print len(x)
+for i in x: print i
+write_data(x)
 
 
 
