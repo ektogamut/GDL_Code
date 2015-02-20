111adce5f09eb1a32db371c98237c20aef7cd5a7
Richard Grant <regrant@gmail.com>
2015-02-19 22:31:04 -0500

    remove global variables

--- 384_data_cleanup.py
+++ 384_data_cleanup.py
@@ -5,26 +5,6 @@ from Tkinter import Tk
 from tkFileDialog import askopenfilename, asksaveasfilename
 
 
-
-# def remove_dup_and_blank(reader):
-#     """
-#     Cleans data given by genescan 384 well to format useful to GDL.
-#     :param writer: a csv module write object
-#     :return: a dictionary {patient1: {gene1: [RQ, RQ_Min, RQ_Max], gene2: [RQ, RQ_Min, RQ_Max]},
-#                            patient2: {gene1: [RQ, RQ_Min, RQ_Max], gene2: [RQ, RQ_Min, RQ_Max]}}
-#     """
-#     data_dict = {}
-#     for sample, gene, rq, rmin, rmax in reader:
-#         if reader.line_num > 38:
-#             # print reader.line_num
-#             if sample not in data_dict and rq:
-#                 data_dict[sample] = {gene: [rq, rmin, rmax]}
-#             elif sample in data_dict and gene not in data_dict[sample] and rq:
-#                 data_dict[sample][gene] = [rq, rmin, rmax]
-#     # for item, value in data_dict.items(): print item, value
-#     return data_dict
-
-
 def remove_dup_and_blank(reader):
     """
     Cleans data given by genescan 384 well to format useful to GDL.
@@ -69,21 +49,19 @@ def write_data(data_set):
     t_output_csv.close()
 
 
-Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-
-# tab_csv = open("/Users/regrant/GDL Code/Source Files/HI DD/ALL 384 well data HHT-2994 c.csv", 'rb')
-tab_csv = open(filename)
-t_reader = csv.reader(tab_csv, dialect='excel-tab')
-
-# t_output_csv = open("test_output.csv", 'wb')
-# writer = csv.writer(t_output_csv, dialect='excel-tab')
-x = remove_dup_and_blank(t_reader)
-
-# print len(x)
-# for i in x: print i
-write_data(x)
+def clean_data():
+    """
+    Runs remove dup and blank as well as writes data
+    """
+    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
+    tab_csv = open(filename)
+    t_reader = csv.reader(tab_csv, dialect='excel-tab')
+    x = remove_dup_and_blank(t_reader)
+    write_data(x)
 
 
+    tab_csv.close()
+    return
 
-tab_csv.close()
+clean_data()
